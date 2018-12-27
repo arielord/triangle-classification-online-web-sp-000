045ec0feb40c8bf08bc19f20d0b1b14d53921172
Ariel <arielord@buffalo.edu>
2018-12-27 14:54:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,7 +5,8 @@ class Triangle
   def initialize(side1, side2, side3)
     if side1 <= 0 || side2 <= 0 || side3 <= 0
       raise TriangleError
-    elsif (self.side1 + self.side2) <= self.side3 || (self.side1 + self.side3) <= self.side2 || (self.side2 + self.side3) <= self.side1
+    end
+    if (self.side1 + self.side2) <= self.side3 || (self.side1 + self.side3) <= self.side2 || (self.side2 + self.side3) <= self.side1
     
       raise TriangleError
     end
